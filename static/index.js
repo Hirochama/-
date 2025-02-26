a2f6cecdec2779bd524cb617cafeab7b5e167161
Hirochama <yh.hirochama@icloud.com>
2025-02-27 07:57:14 +0900

    めんた

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 // ホーム画面で特にJSが必要な場合はここに記述
 // 例: ボタンのクリックでアニメーションなど
 document.addEventListener('DOMContentLoaded', function(){
-    // 今回は特に処理はなし
-    console.log("ホーム画面読み込み完了");
-  });
+  // 今回は特に処理はなし
+  console.log("ホーム画面読み込み完了");
+});
